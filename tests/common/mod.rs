c90db87a55d22886d5e4cc40a0cd2dfa5a9e3c90
Arkadiusz Piekarz <piekarzarkadiusz@gmail.com>
2019-05-26 13:28:31 +0200

    Silence warnings about unused functions and constants in common test module

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,3 +1,8 @@
+// The silence below is needed because each test file is compiled by "cargo test" as a separate crate,
+// but no test file uses every entry from this common module, thus unnecessarily warning
+// about unused functions and constants.
+#![allow(dead_code)]
+
 pub mod accessors;
 pub mod actions;
 pub mod assertions;
