ba30ffaacb7c7b8d78b2a15a5712a56abba1204b
Arkadiusz Piekarz <piekarzarkadiusz@gmail.com>
2019-06-05 21:35:26 +0200

    Add test stageFile

--- tests/common/actions.rs
+++ tests/common/actions.rs
@@ -21,12 +21,28 @@ fn selectFile(filePath: &Path, widgetName: &str, window: &gtk::Widget)
     let widget = gtk_test::find_widget_by_name(window, widgetName).unwrap();
     let treeView = widget.downcast::<gtk::TreeView>().unwrap();
     let model = treeView.get_model().unwrap();
-    let mut selectedSuccessfully = false;
+    let mut rowFound = false;
     model.foreach(|model, _row, iter| {
         if getCell(model, iter, FileStatusModelColumn::Path) != filePath {
             return CONTINUE_ITERATING_MODEL; }
         treeView.get_selection().select_iter(iter);
-        selectedSuccessfully = true;
+        rowFound = true;
         STOP_ITERATING_MODEL });
-    assert_eq!(true, selectedSuccessfully);
+    assert_eq!(true, rowFound);
+}
+
+pub fn activateUnstagedFile(filePath: &Path, window: &gtk::Widget)
+{
+    let filePath = filePath.to_str().unwrap();
+    let widget = gtk_test::find_widget_by_name(window, "Unstaged files view").unwrap();
+    let treeView = widget.downcast::<gtk::TreeView>().unwrap();
+    let model = treeView.get_model().unwrap();
+    let mut rowFound = false;
+    model.foreach(|model, row, iter| {
+        if getCell(model, iter, FileStatusModelColumn::Path) != filePath {
+            return CONTINUE_ITERATING_MODEL; }
+        treeView.row_activated(row, &treeView.get_column(FileStatusModelColumn::Path as i32).unwrap());
+        rowFound = true;
+        STOP_ITERATING_MODEL });
+    assert_eq!(true, rowFound);
 }
---
+++ tests/stage_file.rs
@@ -0,0 +1,51 @@
+#![allow(non_snake_case)]
+
+mod common;
+
+use common::actions::{activateUnstagedFile, selectStagedFile};
+use common::assertions::{
+    assertDiffViewContains,
+    assertDiffViewIsEmpty,
+    assertStagedFilesViewContains,
+    assertStagedFilesViewIsEmpty,
+    assertUnstagedFilesViewContains,
+    assertUnstagedFilesViewIsEmpty
+};
+use common::setup::{getWindow, makeNewUnstagedFile, setupTest};
+use common::utils::FileInfo;
+use rusty_git_gui::app_setup::{makeGtkApp, NO_APP_ARGUMENTS};
+use rusty_git_gui::gui_setup::buildGui;
+use rusty_git_gui::repository::Repository;
+use gio::{ApplicationExt as _, ApplicationExtManual as _};
+use std::path::PathBuf;
+use std::rc::Rc;
+
+
+#[test]
+fn stageFile()
+{
+    let repositoryDir = setupTest();
+    let repositoryDir = repositoryDir.path().to_owned();
+    let filePath = PathBuf::from("fileName");
+    makeNewUnstagedFile(&filePath, "file content\n", &repositoryDir);
+
+    let gtkApp = makeGtkApp();
+    gtkApp.connect_activate(move |gtkApp| {
+        buildGui(gtkApp, Rc::new(Repository::new(&repositoryDir)));
+        let window = getWindow();
+
+        assertUnstagedFilesViewContains(&[FileInfo::new("WT_NEW", &filePath)], &window);
+        assertStagedFilesViewIsEmpty(&window);
+        assertDiffViewContains("@@ -0,0 +1 @@\n+file content\n", &window);
+
+        activateUnstagedFile(&filePath, &window);
+
+        assertUnstagedFilesViewIsEmpty(&window);
+        assertStagedFilesViewContains(&[FileInfo::new("INDEX_NEW", &filePath)], &window);
+        assertDiffViewIsEmpty(&window);
+
+        selectStagedFile(&filePath, &window);
+        assertDiffViewContains("@@ -0,0 +1 @@\n+file content\n", &window);
+    });
+    gtkApp.run(&NO_APP_ARGUMENTS);
+}
