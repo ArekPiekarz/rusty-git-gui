62a5417f6299bcc9cfa444e6868e4f5e031fc5ef
Arkadiusz Piekarz <piekarzarkadiusz@gmail.com>
2019-06-17 20:48:03 +0200

    Add first 4 unit tests, for findRepositoryDir(), using Mocktopus
    Also:
    - require nightly compiler to run tests because of Mocktopus
    - return Result from findRepositoryDir() instead of exiting inside
    - return Result from main()

--- src/app_setup.rs
+++ src/app_setup.rs
@@ -1,8 +1,33 @@
-use crate::error_handling::exit;
+use failchain::ResultExt as _;
 use gio::ApplicationExt as _;
 use std::path::PathBuf;
+#[cfg(test)] use mocktopus::macros::mockable;
+
+
+pub type Error = failchain::BoxedError<ErrorKind>;
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Clone, Eq, PartialEq, Debug, Fail)]
+pub enum ErrorKind {
+    #[fail(display = "Failed to get current directory.")]
+    GetCurrentDirFailed,
+    #[fail(display = "Failed to find a repository directory.")]
+    FindRepositoryDirFailed,
+    #[fail(display = "Too many arguments to the application, expected 0 or 1 (repository directory), \
+                      instead got {}:\n{:?}", 0, 1)]
+    TooManyArgumentsToApp(usize, Vec<String>)
+}
+
+impl failchain::ChainErrorKind for ErrorKind
+{
+    type Error = Error;
+}
+
+use ErrorKind::*;
+
 
 pub const NO_APP_ARGUMENTS : [String; 0] = [];
+
 
 pub fn makeGtkApp() -> gtk::Application
 {
@@ -12,17 +37,76 @@ pub fn makeGtkApp() -> gtk::Application
     gtkApp
 }
 
-pub fn findRepositoryDir() -> PathBuf
+pub fn findRepositoryDir() -> Result<PathBuf>
 {
-    {
-        let args = &std::env::args().collect::<Vec<String>>();
+    (|| -> Result<PathBuf> {
+        let args = getAppArguments();
         match args.len() {
-            1 => std::env::current_dir()
-                .map_err(|e| format_err!("Failed to get current directory: {}", e)),
+            1 => Ok(getCurrentDir()?),
             2 => Ok(PathBuf::from(&args[1])),
-            size => Err(format_err!(
-                "Too many arguments to the application, expected 0 or 1 (repository directory), instead got {}:\n{:?}",
-                size-1, args))
+            size => Err(TooManyArgumentsToApp(size-1, args.clone()).into())
         }
-    }.unwrap_or_else(|e| exit(&format!("Failed to find a repository directory. {}", e)))
+    })().chain_err(|| FindRepositoryDirFailed)
+}
+
+
+#[cfg_attr(test, mockable)]
+fn getAppArguments() -> Vec<String>
+{
+    std::env::args().collect::<Vec<String>>()
+}
+
+#[cfg_attr(test, mockable)]
+fn getCurrentDir() -> Result<PathBuf>
+{
+    std::env::current_dir().chain_err(|| GetCurrentDirFailed)
+}
+
+
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+    use crate::error_handling::formatFail;
+    use mocktopus::mocking::*;
+    use mocktopus::mocking::MockResult::Return;
+
+    #[test]
+    fn findRepositoryDir_shouldReturnCurrentDir_whenNoArgumentToAppIsProvided()
+    {
+        getAppArguments.mock_safe(|| Return(vec!["/path/to/app".into()]));
+        getCurrentDir.mock_safe(|| Return(Ok(PathBuf::from("/current/dir"))));
+        assert_eq!(PathBuf::from("/current/dir"), findRepositoryDir().unwrap());
+    }
+
+    #[test]
+    fn findRepositoryDir_shouldReturnPathFromArgumentToApp_whenOneIsProvided()
+    {
+        getAppArguments.mock_safe(|| Return(vec!["/path/to/app".into(), "/path/to/repository".into()]));
+        assert_eq!(PathBuf::from("/path/to/repository"), findRepositoryDir().unwrap());
+    }
+
+    #[test]
+    fn findRepositoryDir_shouldReturnTooManyArgumentsError_whenMoreThanOneArgumentIsProvided()
+    {
+        getAppArguments.mock_safe(
+            || Return(vec!["/path/to/app".into(), "/path/to/repository".into(), "unknown_argument".into()]));
+        assert_eq!(
+            "error: Failed to find a repository directory.\n  \
+               cause: Too many arguments to the application, expected 0 or 1 (repository directory), instead got 2:\n\
+             [\"/path/to/app\", \"/path/to/repository\", \"unknown_argument\"]",
+            formatFail(&findRepositoryDir().unwrap_err()));
+    }
+
+    #[test]
+    fn findRepositoryDir_shouldReturnCurrentDirError_whenGettingCurrentDirFails()
+    {
+        getAppArguments.mock_safe(|| Return(vec!["/path/to/app".into()]));
+        getCurrentDir.mock_safe(|| Return(Err::<PathBuf,std::io::Error>(std::io::ErrorKind::PermissionDenied.into())
+            .chain_err(|| GetCurrentDirFailed)));
+        assert_eq!("error: Failed to find a repository directory.\n  \
+                      cause: Failed to get current directory.\n  \
+                      cause: permission denied",
+                   formatFail(&findRepositoryDir().unwrap_err()));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,21 @@
 #![allow(non_snake_case)]
 #![deny(unused_must_use)]
 
-use rusty_git_gui::app_setup::{findRepositoryDir, makeGtkApp, NO_APP_ARGUMENTS};
+use rusty_git_gui::app_setup::{findRepositoryDir, makeGtkApp, NO_APP_ARGUMENTS, Result};
 use rusty_git_gui::gui_setup::buildGui;
 use rusty_git_gui::repository::Repository;
+
 use gio::ApplicationExt as _;
 use gio::ApplicationExtManual as _;
 use std::rc::Rc;
 
 
-fn main()
+fn main() -> Result<()>
 {
     color_backtrace::install();
     let gtkApp = makeGtkApp();
-    gtkApp.connect_activate(|gtkApp| buildGui(gtkApp, Rc::new(Repository::new(&findRepositoryDir()))));
+    let repositoryDir = findRepositoryDir()?;
+    gtkApp.connect_activate(move |gtkApp| buildGui(gtkApp, Rc::new(Repository::new(&repositoryDir))));
     gtkApp.run(&NO_APP_ARGUMENTS);
+    Ok(())
 }
