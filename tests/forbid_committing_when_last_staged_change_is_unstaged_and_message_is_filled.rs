689a8aceb52a146cbfc07a7ee5fe9084b9f92fde
Arkadiusz Piekarz <piekarzarkadiusz@gmail.com>
2019-10-21 21:17:53 +0200

    Add integration test forbidCommittingWhenLastStagedChangeIsUnstagedAndMessageIsFilled

---
+++ tests/forbid_committing_when_last_staged_change_is_unstaged_and_message_is_filled.rs
@@ -0,0 +1,33 @@
+#![allow(non_snake_case)]
+
+mod common;
+
+use common::gui_assertions::{
+    assertCommitButtonIsEnabled,
+    assertCommitButtonIsDisabled,
+    assertCommitButtonTooltipIs,
+    assertCommitButtonTooltipIsEmpty};
+use common::gui_interactions::{activateStagedChangeToUnstageIt, setCommitMessage};
+use common::setup::{makeGui, makeNewStagedFile, setupTest};
+
+use std::path::PathBuf;
+
+
+#[test]
+fn forbidCommittingWhenLastStagedChangeIsUnstagedAndMessageIsFilled()
+{
+    let repositoryDir = setupTest();
+    let repositoryDir = repositoryDir.path().to_owned();
+    let filePath = PathBuf::from("file");
+    makeNewStagedFile(&filePath, "staged file content\n", &repositoryDir);
+
+    let gui = makeGui(&repositoryDir);
+    setCommitMessage("some commit message", &gui);
+    assertCommitButtonIsEnabled(&gui);
+    assertCommitButtonTooltipIsEmpty(&gui);
+
+    activateStagedChangeToUnstageIt(&filePath, &gui);
+
+    assertCommitButtonIsDisabled(&gui);
+    assertCommitButtonTooltipIs("No changes are staged for commit.", &gui);
+}
