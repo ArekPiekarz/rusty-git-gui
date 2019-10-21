f2ff37385959dce267027542af5f8f486e71816c
Arkadiusz Piekarz <piekarzarkadiusz@gmail.com>
2019-10-21 22:17:18 +0200

    Add integration test unstageDeletedFile

---
+++ tests/unstage_deleted_file.rs
@@ -0,0 +1,72 @@
+#![allow(non_snake_case)]
+
+mod common;
+
+use common::file_change_view_utils::makeFileChange;
+use common::gui_assertions::{
+    assertDiffViewContains,
+    assertDiffViewIsEmpty,
+    assertStagedChangesViewContains,
+    assertStagedChangesViewIsEmpty,
+    assertUnstagedChangesViewContains,
+    assertUnstagedChangesViewIsEmpty};
+use common::gui_interactions::{activateStagedChangeToUnstageIt, selectUnstagedChange};
+use common::repository_assertions::{assertRepositoryLogIs, assertRepositoryStatusIs};
+use common::repository_status_utils::{FileChangeStatus::*, RepositoryStatusEntry as Entry};
+use common::setup::{makeCommit, makeGui, makeNewStagedFile, removeFile, setupTest, stageFile};
+
+use std::path::PathBuf;
+
+
+#[test]
+fn unstageDeletedFile()
+{
+    let repositoryDir = setupTest();
+    let repositoryDir = repositoryDir.path().to_owned();
+    let filePath = PathBuf::from("fileName");
+    makeNewStagedFile(&filePath, "some file content\nsecond line\n", &repositoryDir);
+    makeCommit("Initial commit", &repositoryDir);
+    removeFile(&filePath, &repositoryDir);
+    stageFile(&filePath, &repositoryDir);
+
+    let gui = makeGui(&repositoryDir);
+
+    assertRepositoryStatusIs(
+        &[Entry{path: filePath.clone(), workTreeStatus: Unmodified, indexStatus: Deleted}],
+        &repositoryDir);
+    assertRepositoryLogIs(REPOSITORY_LOG, &repositoryDir);
+    assertUnstagedChangesViewIsEmpty(&gui);
+    assertStagedChangesViewContains(&[makeFileChange("INDEX_DELETED", &filePath)], &gui);
+    assertDiffViewIsEmpty(&gui);
+
+    activateStagedChangeToUnstageIt(&filePath, &gui);
+
+    assertRepositoryStatusIs(
+        &[Entry{path: filePath.clone(), workTreeStatus: Deleted, indexStatus: Unmodified}],
+        &repositoryDir);
+    assertRepositoryLogIs(REPOSITORY_LOG, &repositoryDir);
+    assertUnstagedChangesViewContains(&[makeFileChange("WT_DELETED", &filePath)], &gui);
+    assertStagedChangesViewIsEmpty(&gui);
+    assertDiffViewIsEmpty(&gui);
+
+    selectUnstagedChange(&filePath, &gui);
+    assertDiffViewContains("@@ -1,2 +0,0 @@\n-some file content\n-second line\n", &gui);
+}
+
+const REPOSITORY_LOG: &str =
+    r#"Author: John Smith
+Email: john.smith@example.com
+Subject: Initial commit
+---
+ fileName | 2 ++
+ 1 file changed, 2 insertions(+)
+
+diff --git a/fileName b/fileName
+new file mode 100644
+index 0000000..1820ab1
+--- /dev/null
++++ b/fileName
+@@ -0,0 +1,2 @@
++some file content
++second line
+"#;
